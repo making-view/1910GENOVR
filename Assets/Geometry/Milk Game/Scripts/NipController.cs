527fbc79ae310f55f18287cde6252f309f3a166f
Marius Stubberud <mars@makingview.com>
2019-12-03 13:11:16 +0100

    Cow milking WIP

---
+++ Assets/Geometry/Milk Game/Scripts/NipController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NipController : MonoBehaviour
+{
+    [SerializeField] private float maxThreshold = 80f;
+    [SerializeField] private GameObject tittyBone = null;
+    [SerializeField] private ParticleSystem tittyMilk = null;
+
+    private Vector3 tittyStartPos;
+    private Vector3 controlObjectStartPos;
+    private float thresholdFactor = 1000.0f;
+
+    void Start()
+    {
+        controlObjectStartPos = transform.position;
+        tittyStartPos = tittyBone.transform.position;
+
+        var emission = tittyMilk.emission;  
+        emission.rateOverTime = 0.0f;
+    }
+
+    void Update()
+    {
+        var threshold = maxThreshold / thresholdFactor;
+        var diff = Mathf.Clamp((controlObjectStartPos - transform.position).y, 0.0f, threshold);
+        var newPos = tittyBone.transform.position;
+        newPos.y = tittyStartPos.y - diff;
+
+        tittyBone.transform.position = newPos;
+
+        var emission = tittyMilk.emission;
+        if (diff == threshold)
+            emission.rateOverTime = 10.0f;
+        else
+            emission.rateOverTime = 0.0f;
+    }
+}
