25bf79d9348ac6ecdb7ed22d882afccece143dd1
Marius Stubberud <mars@makingview.com>
2019-12-05 12:24:03 +0100

    Milking now finite and only once per grab

--- Assets/Geometry/Milk Game/Scripts/NipController.cs
+++ Assets/Geometry/Milk Game/Scripts/NipController.cs
@@ -7,18 +7,23 @@ public class NipController : MonoBehaviour
 {
     [SerializeField] private float maxThreshold = 80f;
     [SerializeField] private float minThreshold = -20f;
+    [SerializeField] private float maxMilkTime = 0.3f;
     [SerializeField] private GameObject tittyBone = null;
     [SerializeField] private ParticleSystem tittyMilk = null;
 
     private Vector3 tittyStartPos;
-    private Vector3 controlObjectStartPos;
+    private Vector3 controllerStartPos;
+    private Quaternion controllerStartRot;
     private float thresholdFactor = 1000.0f;
     private OVRGrabbable grabbable = null;
     private bool previouslyGrabbed = false;
+    private float timer = 0.0f;
+    private bool trySquirting = false;
 
     void Start()
     {
-        controlObjectStartPos = transform.position;
+        controllerStartPos = transform.position;
+        controllerStartRot = transform.rotation;
         tittyStartPos = tittyBone.transform.position;
         grabbable = GetComponent<OVRGrabbable>();
 
@@ -30,23 +35,32 @@ public class NipController : MonoBehaviour
     {
         if (!grabbable.isGrabbed && previouslyGrabbed)
         {
-            transform.position = controlObjectStartPos;
+            transform.position = controllerStartPos;
+            transform.rotation = controllerStartRot;
         }
 
         var maxDiff = maxThreshold / thresholdFactor;
         var minDiff = minThreshold / thresholdFactor;
-        var diff = Mathf.Clamp((controlObjectStartPos - transform.position).y, minDiff, maxDiff);
+        var diff = Mathf.Clamp((controllerStartPos - transform.position).y, minDiff, maxDiff);
         var newPos = tittyBone.transform.position;
         newPos.y = tittyStartPos.y - diff;
 
         tittyBone.transform.position = newPos;
 
+        trySquirting = diff == maxDiff;
         var emission = tittyMilk.emission;
-        if (diff == maxDiff)
-            emission.rateOverTime = 10.0f;
-        else
-            emission.rateOverTime = 0.0f;
+        emission.rateOverTime = 0.0f;
 
-        previouslyGrabbed = grabbable.isGrabbed;  
+        if (trySquirting && timer <= maxMilkTime)
+        {
+            emission.rateOverTime = 10.0f;
+            timer += Time.deltaTime;
+        }
+        else if (!grabbable.isGrabbed)
+        {
+            timer = 0.0f;
+        }
+
+        previouslyGrabbed = grabbable.isGrabbed;
     }
 }
