908ed32c11e363005f56fef9287059f50d21b573
Marius Stubberud <mars@makingview.com>
2019-12-12 10:13:42 +0100

    GReeaaargh

--- Assets/Scripts/ChainAction.cs
+++ Assets/Scripts/ChainAction.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class ChainAction : MonoBehaviour
 {
+    private Vector3 startPos;
+    private Quaternion startRot;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.position;
+        startRot = transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.rotation = startRot;
+        transform.position = new Vector3(startPos.x, transform.position.y, startPos.z);
     }
 }
